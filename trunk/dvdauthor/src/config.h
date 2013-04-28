ba29a9b5cc80ce518de3e27b97904dc7d8be6a23
aoanla@gmail.com <aoanla@gmail.com@88fbaa10-0dfd-12ad-a2f6-fbabcff00ebe>
2013-04-28 08:12:47 +0000

    Adding patched dvdauthor code to new directory

---
+++ trunk/dvdauthor/src/config.h
@@ -0,0 +1,123 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Default video format, 1 => NTSC, 2 => PAL */
+/* #undef DEFAULT_VIDEO_FORMAT */
+
+/* Define to 1 if you have the declaration of `O_BINARY', and to 0 if you
+   don't. */
+#define HAVE_DECL_O_BINARY 0
+
+/* whether Fontconfig is available */
+/* #undef HAVE_FONTCONFIG */
+
+/* Whether FreeType is available */
+#define HAVE_FREETYPE 1
+
+/* whether FriBidi is available */
+/* #undef HAVE_FRIBIDI */
+
+/* Define to 1 if you have the <ft2build.h> header file. */
+#define HAVE_FT2BUILD_H 1
+
+/* Define to 1 if you have the <getopt.h> header file. */
+#define HAVE_GETOPT_H 1
+
+/* Define to 1 if you have the `getopt_long' function. */
+#define HAVE_GETOPT_LONG 1
+
+/* Whether the GraphicsMagick libraries are available */
+/* #undef HAVE_GMAGICK */
+
+/* Define if you have the iconv() function and it works. */
+#define HAVE_ICONV 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <io.h> header file. */
+/* #undef HAVE_IO_H */
+
+/* Define to 1 if you have the `gnugetopt' library (-lgnugetopt). */
+/* #undef HAVE_LIBGNUGETOPT */
+
+/* Whether the ImageMagick libraries are available */
+/* #undef HAVE_MAGICK */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* define to 1 if C compiler allows nested routines */
+#define HAVE_NESTED_ROUTINES 1
+
+/* Define to 1 if you have the `setmode' function. */
+/* #undef HAVE_SETMODE */
+
+/* Define to 1 if stdbool.h conforms to C99. */
+#define HAVE_STDBOOL_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_STRNDUP 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+#define HAVE__BOOL 1
+
+/* Define as const if the declaration of iconv() needs const. */
+#define ICONV_CONST 
+
+/* Whether to localize filenames */
+/* #undef LOCALIZE_FILENAMES */
+
+/* Name of package */
+#define PACKAGE "dvdauthor"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "dvdauthor-users@lists.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "DVDAuthor"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "DVDAuthor 0.7.1+"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "dvdauthor"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.7.1+"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.7.1+"
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* Define for large files, on AIX-style hosts. */
+/* #undef _LARGE_FILES */
